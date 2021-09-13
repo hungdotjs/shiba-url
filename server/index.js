ee6b955e5c47fade188dc89506f59bff40186dc1
Hung Tran <hungteddy98@gmail.com>
2021-09-13 21:08:32 +0700

    fix deploy err

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ app.use(express.json({ extended: false }));
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
-const PORT = 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on PORT ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
